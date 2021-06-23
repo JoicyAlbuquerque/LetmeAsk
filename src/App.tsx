b1d1ad3d62ac342c78c2da3cb0a789b423fcdb08
Joicy Albuquerque <dev12@pluthink.com>
2021-06-23 12:07:31 -0300

    Auth - Home - NewRoom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import {Button} from './components/Button'
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
 
-import './services/firebase';
+import {BrowserRouter, Route} from 'react-router-dom';
+import {AuthContextProvider} from  './contexts/AuthContext'
+//import './services/firebase';
 
 function App() {
+
   return (
-    <div>
-      <Button />
-      <Button />
-      <Button />
-    </div>
+  <BrowserRouter>
+    <AuthContextProvider>
+      <Route path="/" exact component={Home} />
+      <Route path="/room/new" component={NewRoom} />
+    </AuthContextProvider>
+  </BrowserRouter>
   );
 }
 
